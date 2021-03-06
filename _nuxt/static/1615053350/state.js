08b0ab83803c47a24b2e54e830f85197aec6d4b8
Michael Tandy <mike.tandy.1@gmail.com>
2021-03-06 17:55:56 +0000

    Updates

---
+++ _nuxt/static/1615053350/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fvideo-browser\u002F_nuxt\u002Fstatic\u002F1615053350",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fvideo-browser\u002F",assetsPath:"\u002Fvideo-browser\u002F_nuxt\u002F",cdnURL:a}}}}(null));
