952b117ada493ae9f9ecaaf6a4ad19e815db470e
Michael Tandy <mike.tandy.1@gmail.com>
2021-03-06 18:17:50 +0000

    edited nust config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,5 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  target: "static",
-  router: {
-    base: "/video-browser/"
-  },
 
   head: {
     title: "video-browser",
